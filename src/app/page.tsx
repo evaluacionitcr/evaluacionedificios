00660f1e538abfb0ec9687b236db441c2e316d1f
javialroro <102406440+javialroro@users.noreply.github.com>
2025-03-14 12:09:28 -0600

    Updated sign in component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
 
   void api.post.getLatest.prefetch();
+  
 
   return (
     <ProtectedRoute>
