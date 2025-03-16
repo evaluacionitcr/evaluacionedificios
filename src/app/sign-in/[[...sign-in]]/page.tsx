f8121a319cf56f52d5a15d60bc1e65db1a51f99a
Carlos Venegas <cvenegasm1043@gmail.com>
2025-03-16 17:10:59 -0600

    Pantalla Sign-in
    Se realizo la pantalla de Sign-in con fondo y header

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -3,6 +3,20 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
+      <div
+        className="absolute inset-0 bg-black bg-opacity-50 bg-cover bg-center"
+        style={{ 
+          backgroundImage: "url('/OficinaIng.JPG')",
+          filter: "brightness(50%)" }}
+      ></div>
+      <header 
+        className="absolute top-0 left-0 w-full p-4 grid grid-cols-3 items-center"
+        style={{ background: "#808080" }}>
+        <img src="/tec-logo.png" alt="TEC Logo" className="h-10 ml-4" />
+        <h1 className="text-white text-lg font-bold text-center">Oficina de Ingeniería</h1>
+        <div></div>
+      </header>
+
       <SignIn
         appearance={{
           elements: {
@@ -10,6 +24,7 @@ export default function Page() {
           },
         }}
       />
+      
     </div>
   );
 }
