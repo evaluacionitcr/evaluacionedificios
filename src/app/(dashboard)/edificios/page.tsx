4a0021dceae04f019924ca60006cc99bdaf31aa7
Javier Rojas <102406440+javialroro@users.noreply.github.com>
2025-04-29 23:46:33 -0600

    Update page.tsx

--- src/app/(dashboard)/edificios/page.tsx
+++ src/app/(dashboard)/edificios/page.tsx
@@ -1,6 +1,8 @@
 import BuildingsView from "~/components/views/buildings-view"
+// BuildingsView is a component that displays a list of buildings 
 
 export default function EdificiosPage() {
   return <BuildingsView />
+
 }
 
